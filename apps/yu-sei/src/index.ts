d6f433f8ddb1b980ee0bba4922dd97c52cc33d12
erichong <f102041332@gmail.com>
2024-05-12 01:07:38 +0800

    調整 package.json

--- apps/yu-sei/src/index.ts
+++ apps/yu-sei/src/index.ts
@@ -1,7 +1,18 @@
 import figlet from 'figlet';
+import { DataAccessService } from '@ygo/mongo-server';
 
-console.log(
-  figlet.textSync('YGO Reptile!!!', {
-    font: 'Ghost',
-  })
-);
+const main = async () => {
+  console.log(
+    figlet.textSync('YGO Reptile!!!', {
+      font: 'Ghost',
+    })
+  );
+
+  const daService = new DataAccessService();
+  await daService.init();
+
+  const da = await daService.find('admin', { type: 0 });
+  console.log(da);
+};
+
+main();
--- libs/mongo-server/src/index.ts
+++ libs/mongo-server/src/index.ts
@@ -1,5 +1,4 @@
-import { ModelNames, ModelSchema } from '@ygo/schemas';
 import { DataAccessService } from './dataAccess/mongo';
 import { modelSchemas } from './dataAccess/model-schemas';
 
-export { ModelNames, ModelSchema, DataAccessService, modelSchemas };
+export { DataAccessService, modelSchemas };
