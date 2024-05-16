54e62d71363449f1a1117f81b2a57fff6644ab72
erichong <erichong@91app.com>
2024-05-16 15:59:39 +0800

    add envRunner

---
+++ apps/yu-sei/src/app.ts
@@ -0,0 +1,7 @@
+import { config } from 'dotenv';
+import { resolve } from 'path';
+
+export const envRunner = () => {
+  config({ path: resolve(__dirname, '../../config/.env.common') });
+  config({ path: resolve(__dirname, '../../config/.env.yusei') });
+};
--- apps/yu-sei/src/index.ts
+++ apps/yu-sei/src/index.ts
@@ -1,8 +1,10 @@
+import { envRunner } from './app';
 import figlet from 'figlet';
 import { DataAccessService } from '@ygo/mongo-server';
 import { CardsDataType } from '@ygo/schemas';
 
 const main = async () => {
+  envRunner();
   console.log(
     figlet.textSync('YGO Reptile!!!', {
       font: 'Ghost',
