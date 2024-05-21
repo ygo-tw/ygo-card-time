c232582e793d8832808dccbf93d89e5d81df56ef
erichong <f102041332@gmail.com>
2024-05-21 22:17:12 +0800

    test jest on pre-push

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   '*.{js,jsx,ts,tsx,json,css,scss,md,vue}': ['prettier --write'],
-  '*.{js,jsx,ts,tsx,vue}': [
-    'eslint --fix',
-    'jest --findRelatedTests --passWithNoTests',
-  ],
+  '*.{js,jsx,ts,tsx,vue}': ['eslint --fix'],
   '*.{ts,tsx}': ['node ./.husky/check-tsc.js'],
 };
