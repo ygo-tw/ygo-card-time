35f8e72a1f8cb4c284273c95e050b94301c656a2
erichong <f102041332@gmail.com>
2024-05-22 23:42:55 +0800

    補充 mailer unit test & try catch

--- libs/mailer/src/service/mailer.test.ts
+++ libs/mailer/src/service/mailer.test.ts
@@ -30,6 +30,33 @@ describe('YGOMailer', () => {
     console.error = originalConsoleError; // 恢复 console.error
   });
 
+  describe('constructor', () => {
+    it('should initialize transporter successfully', () => {
+      expect(mockedNodemailer.createTransport).toHaveBeenCalledWith({
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false,
+        auth: {
+          user: process.env.EMAIL,
+          pass: process.env.EPASSWORD,
+        },
+      });
+    });
+
+    it('should log an error and throw if transporter initialization fails', () => {
+      const error = new Error('Initialization failed');
+      mockedNodemailer.createTransport.mockImplementationOnce(() => {
+        throw error;
+      });
+
+      expect(() => new YGOMailer()).toThrow('Failed to initialize transporter');
+      expect(console.error).toHaveBeenCalledWith(
+        'Failed to initialize transporter:',
+        error
+      );
+    });
+  });
+
   describe('reInit method', () => {
     it('should reinitialize the transporter with new credentials', () => {
       const newEmail = 'new@example.com';
@@ -44,6 +71,21 @@ describe('YGOMailer', () => {
           pass: newPassword,
         },
       });
+    });
+
+    it('should log an error and throw if reinitialization fails', () => {
+      const error = new Error('Reinitialization failed');
+      mockedNodemailer.createTransport.mockImplementationOnce(() => {
+        throw error;
+      });
+
+      expect(() => mailer.reInit('new@example.com', 'newpassword123')).toThrow(
+        'Failed to reinitialize transporter'
+      );
+      expect(console.error).toHaveBeenCalledWith(
+        'Failed to reinitialize transporter:',
+        error
+      );
     });
   });
 
@@ -70,6 +112,22 @@ describe('YGOMailer', () => {
       const errorMessage = 'Failed to send email';
       mockTransport.sendMail.mockRejectedValue(new Error(errorMessage));
       await expect(mailer.sendMail(mailOptions)).rejects.toThrow(errorMessage);
+    });
+
+    it('should throw an error if transporter is not initialized', async () => {
+      mailer = new YGOMailer();
+      mailer['transporter'] = undefined; // Simulate transporter being undefined
+
+      const mailOptions = {
+        from: 'test@example.com',
+        to: 'receiver@example.com',
+        subject: 'Test',
+        text: 'This is a test email',
+      };
+
+      await expect(mailer.sendMail(mailOptions)).rejects.toThrow(
+        'Transporter is not initialized'
+      );
     });
   });
 });
