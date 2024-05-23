602f8409054c3d259f0f012fa7780f6e8ac56378
erichong <f102041332@gmail.com>
2024-05-23 22:36:51 +0800

    test

--- apps/yu-sei/src/index.ts
+++ apps/yu-sei/src/index.ts
@@ -5,6 +5,7 @@ import { DataAccessService } from '@ygo/mongo-server';
 // import { RutenService } from './cronJobs/rutenPrice';
 import { CheerioCrawler } from '@ygo/crawler';
 import { YgoJpInfo } from './cronJobs/ygoJpInfo';
+import { CustomLogger } from './utils';
 
 const main = async () => {
   envRunner();
@@ -18,12 +19,13 @@ const main = async () => {
   const da = new DataAccessService(
     `mongodb+srv://${process.env.ADMIN}:${process.env.PASSWORD}@cluster0.rnvhhr4.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`
   );
+  const logger = new CustomLogger();
   // const priceCalculator = new PriceCalculator();
   // const returnService = new RutenService(da, priceCalculator);
 
   // await returnService.getRutenPrice();
 
-  const ygoJpInfo = new YgoJpInfo(cheerioCrawler, da);
+  const ygoJpInfo = new YgoJpInfo(cheerioCrawler, da, logger);
 
   await ygoJpInfo.updateCardsJPInfo(['10004783']);
 };
