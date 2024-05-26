84c0ba30c4330a2a36c01ac388a5161f8584d806
erichong <f102041332@gmail.com>
2024-05-26 22:21:56 +0800

    測試購買最佳解

--- apps/yu-sei/src/index.ts
+++ apps/yu-sei/src/index.ts
@@ -1,11 +1,7 @@
 import { envRunner } from './app';
 import figlet from 'figlet';
-import { DataAccessService } from '@ygo/mongo-server';
-// import { PriceCalculator } from './utils';
-// import { RutenService } from './cronJobs/rutenPrice';
-import { CheerioCrawler } from '@ygo/crawler';
-import { YgoJpInfo } from './cronJobs/ygoJpInfo';
-import { CustomLogger } from './utils';
+import fs from 'fs';
+import { BestPlanByRutenService, Shop } from './utils/bestPlanByRutenService';
 
 const main = async () => {
   envRunner();
@@ -15,19 +11,53 @@ const main = async () => {
     })
   );
 
-  const cheerioCrawler = new CheerioCrawler('https://www.db.yugioh-card.com');
-  const da = new DataAccessService(
-    `mongodb+srv://${process.env.ADMIN}:${process.env.PASSWORD}@cluster0.rnvhhr4.mongodb.net/${process.env.DB}?retryWrites=true&w=majority`
+  const shoppingList = [
+    { productName: 'apple', count: 3 },
+    { productName: 'banana', count: 2 },
+    { productName: 'orange', count: 1 },
+    { productName: 'pear', count: 2 },
+    { productName: 'grape', count: 1 },
+  ];
+  const shops: Shop[] = [
+    {
+      id: 'shop1',
+      products: {
+        apple: { price: 80, id: '1', qtl: 3 },
+        banana: { price: 60, id: '2', qtl: 1 },
+      },
+      shipPrices: { seven: 60, family: 45 },
+      freeShip: { seven: 100, family: 80 },
+    },
+    {
+      id: 'shop2',
+      products: {
+        banana: { price: 50, id: '3', qtl: 2 },
+        orange: { price: 80, id: '4', qtl: 1 },
+        grape: { price: 60, id: '9', qtl: 1 },
+      },
+      shipPrices: { seven: 70, family: 40 },
+      freeShip: { seven: 120, family: 110 },
+    },
+    {
+      id: 'shop3',
+      products: {
+        apple: { price: 70, id: '5', qtl: 1 },
+        banana: { price: 55, id: '8', qtl: 1 },
+        orange: { price: 70, id: '6', qtl: 1 },
+        pear: { price: 70, id: '7', qtl: 1 },
+      },
+      shipPrices: { seven: 50, family: 55 },
+      freeShip: { seven: 150, family: 100 },
+    },
+  ];
+  fs.writeFileSync(
+    'result.json',
+    JSON.stringify(
+      BestPlanByRutenService.getBestPlan(shops, shoppingList),
+      null,
+      2
+    )
   );
-  const logger = new CustomLogger();
-  // const priceCalculator = new PriceCalculator();
-  // const returnService = new RutenService(da, priceCalculator);
-
-  // await returnService.getRutenPrice();
-
-  const ygoJpInfo = new YgoJpInfo(cheerioCrawler, da, logger);
-
-  await ygoJpInfo.updateCardsJPInfo(['10004783']);
 };
 
 main();
