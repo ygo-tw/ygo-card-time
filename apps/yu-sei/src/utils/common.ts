3e0fcc3a44192b202a517ee83342ab0601842c55
erichong <f102041332@gmail.com>
2024-06-18 22:49:28 +0800

    add mail payload

--- apps/yu-sei/src/utils/common.ts
+++ apps/yu-sei/src/utils/common.ts
@@ -1,5 +1,27 @@
+import { SendMailOptions } from 'nodemailer';
+
 export const delay = async (time: number) => {
   return new Promise(resolve => {
     setTimeout(resolve, time);
   });
+};
+
+export const makeMailOptionsPayload = (
+  now: string,
+  result: object | [],
+  html: string,
+  filename: string
+): SendMailOptions => {
+  return {
+    from: 'ygo.cardtime@gmail.com',
+    to: 'f102041332@gmail.com,alex8603062000@gmail.com',
+    subject: `爬蟲完成確認信件(${now})`,
+    html,
+    attachments: [
+      {
+        filename,
+        content: JSON.stringify(result, null, 2),
+      },
+    ],
+  };
 };
