91bae0ff6b4e2b2484d18164e2d8522c4aa6cab0
alex1234567639 <alex8603062000@gmail.com>
2024-07-24 13:18:25 +0800

    [modify]cookie註冊的部分放到/plugins

--- apps/kai-ba/src/routes/api/cookieTest.ts
+++ apps/kai-ba/src/routes/api/cookieTest.ts
@@ -1,17 +1,8 @@
 import { FastifyPluginAsync, FastifyInstance } from 'fastify';
-import cookie, { FastifyCookieOptions } from '@fastify/cookie';
-
-// @fastify-cookie 官方說明 https://github.com/fastify/fastify-cookie
 
 const cookieWorkspaces: FastifyPluginAsync = async (
   fastify: FastifyInstance
 ): Promise<void> => {
-  // 用於簽名 cookie 的密鑰
-  fastify.register(cookie, {
-    secret: 'my-secret', // 用於簽名 cookie 的密鑰
-    parseOptions: {}, // 用於解析 cookie 的選項
-  } as FastifyCookieOptions);
-
   // http://localhost:3000/api/set-cookie
   fastify.get('/set-cookie', (_request, reply) => {
     reply
