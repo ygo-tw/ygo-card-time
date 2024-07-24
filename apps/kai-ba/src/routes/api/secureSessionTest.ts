94aaad6ac56e56269e87d62ddeb7f536656938e6
alex1234567639 <alex8603062000@gmail.com>
2024-07-24 13:18:32 +0800

    [modify]session註冊的部分放到/plugins

--- apps/kai-ba/src/routes/api/secureSessionTest.ts
+++ apps/kai-ba/src/routes/api/secureSessionTest.ts
@@ -1,29 +1,8 @@
 import { FastifyPluginAsync, FastifyInstance } from 'fastify';
-import fastifySecureSession from '@fastify/secure-session';
-
-// @fastify/secure-session 官方說明 https://github.com/fastify/fastify-secure-session
-declare module '@fastify/secure-session' {
-  interface SessionData {
-    data: string;
-  }
-}
 
 const sessionWorkspaces: FastifyPluginAsync = async (
   fastify: FastifyInstance
 ): Promise<void> => {
-  fastify.register(fastifySecureSession, {
-    cookieName: 'session',
-    key: Buffer.from(
-      '4fe91796c30bd989d95b62dc46c7c3ba0b6aa2df2187400586a4121c54c53b85',
-      'hex'
-    ),
-    expiry: 60, // 60秒
-    cookie: {
-      path: '/',
-      secure: false, // 設為 true 以使用 HTTPS
-    },
-  });
-
   // http://localhost:3000/api/set-session
   fastify.get('/set-session', (request, reply) => {
     const body = { data: 'example data' };
