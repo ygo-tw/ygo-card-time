07a831e8fa29743e0996235bf57b641cc71ee9f7
alex1234567639 <alex8603062000@gmail.com>
2024-07-25 11:57:44 +0800

    [feature]新增cors plugin

---
+++ apps/kai-ba/src/plugins/cors.ts
@@ -0,0 +1,15 @@
+import fp from 'fastify-plugin';
+import fastifyCors from '@fastify/cors';
+import { FastifyPluginAsync } from 'fastify';
+
+export default fp<FastifyPluginAsync>(
+  async fastify => {
+    fastify.register(fastifyCors, {
+      origin: true,
+      methods: ['GET', 'PUT', 'POST', 'DELETE'],
+    });
+  },
+  {
+    name: 'cors-plugin',
+  }
+);
