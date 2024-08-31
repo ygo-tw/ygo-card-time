6efe02e2bad541f6a98a5723295c13da39c81c3b
alex1234567639 <alex8603062000@gmail.com>
2024-08-31 13:19:13 +0800

    [feature]新增banner json shcema

--- libs/schemas/src/useful/database/index.ts
+++ libs/schemas/src/useful/database/index.ts
@@ -2,6 +2,8 @@ export * from './cards-data.const';
 export * from './cards-data';
 export * from './admin-data';
 export * from './admin-data.const';
+export * from './banner-data';
+export * from './banner-data.const';
 export * from './jurisprudence-data';
 export * from './jurisprudence-data.const';
 export * from './permission-data';
