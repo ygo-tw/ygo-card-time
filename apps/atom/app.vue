fe42907335db19e65a2b3dbfe525d44a120b6191
alex1234567639 <alex8603062000@gmail.com>
2025-06-29 09:37:08 +0800

    [feature]新增footer組件

--- apps/atom/app.vue
+++ apps/atom/app.vue
@@ -2,5 +2,6 @@
   <div>
     <Header />
     <NuxtPage />
+    <Footer />
   </div>
 </template>
---
+++ apps/atom/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="footer-wrapper">
+    Copyright © 卡壇 Card Time. All rights reserved.
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: 'FooterComponent',
+});
+</script>
+
+<style lang="scss" scoped>
+.footer-wrapper {
+  @apply custom-body-s w-full text-center text-white py-3 bg-primary;
+}
+</style>
